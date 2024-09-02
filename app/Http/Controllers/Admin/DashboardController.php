b3c4ef9403c32e692677d2123b3bf61fbb1f228f
Dev Tung <tungcotdo@gmail.com>
2024-09-02 13:32:06 +0700

    update code

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,18 +10,7 @@ use Carbon\Carbon;
 class DashboardController extends Controller
 {
     public function index(){
-        $compact['saleNumber'] = DB::table('sale')->whereIn('sale_status', [1,2,3])->count();
-        $compact['rentNumber'] = DB::table('rent')->whereIn('rent_status', [1,2,3])->count();
-        $compact['saletranNumber'] = DB::table('sale')->whereIn('sale_status', [3])->count();
-        $compact['rentranNumber'] = DB::table('rent')->whereIn('rent_status', [3])->count();
-        $compact['saleRevenueNumber'] = DB::table('sale')->whereIn('sale_status', [4])->sum('sale_price');
-        $compact['rentRevenueNumber'] = DB::table('rent')->whereIn('rent_status', [4])->sum('rent_price');
-        $compact['saleRaws'] = DB::table('sale')->where('sale_status', 1)->limit(10)->get();
-        $compact['saleSelects'] = DB::table('sale')->where('sale_status', 2)->limit(10)->get();
-        $compact['rentRaws'] = DB::table('rent')->where('rent_status', 1)->limit(10)->get();
-        $compact['rentSelects'] = DB::table('rent')->where('rent_status', 2)->limit(10)->get();
-        $compact['notifications'] = DB::table('notification')->limit(10)->get();
-        return view('admin.dashboard.index', $compact);
+        return view('admin.dashboard.index');
     }
 
     public function render(){
